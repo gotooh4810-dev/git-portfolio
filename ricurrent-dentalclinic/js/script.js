61cc891f5ba39fec84077d416c6a817171e4198b
gotooh4810-dev <gotooh4810@gmail.com>
2026-03-22 02:10:10 +0900

    スマフォ時のバーガーメニュー作成

--- ricurrent-dentalclinic/js/script.js
+++ ricurrent-dentalclinic/js/script.js
@@ -1,3 +1,11 @@
+const burgerBtn = document.querySelector('.burger-btn');
+const headerContents = document.querySelector('.header-contents');
+
+burgerBtn.addEventListener('click',() => {
+    burgerBtn.classList.toggle('is-open');
+    headerContents.classList.toggle('is-open');
+});
+
 const questionTtls = document.querySelectorAll('.question-ttl');
 for(let i = 0; i<questionTtls.length; i++){
     questionTtls[i].addEventListener('click',() => {
