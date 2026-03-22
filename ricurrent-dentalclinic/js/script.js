dd502ba53b89c52643243965e85fcaf245712ee5
gotooh4810-dev <gotooh4810@gmail.com>
2026-03-22 15:41:57 +0900

    ハンバーガーメニューないのナビゲーションメニュークリックで.is-openを外す処理

--- ricurrent-dentalclinic/js/script.js
+++ ricurrent-dentalclinic/js/script.js
@@ -1,10 +1,25 @@
 const burgerBtn = document.querySelector('.burger-btn');
 const headerContents = document.querySelector('.header-contents');
+const navLinks = document.querySelectorAll('nav a');
 
 burgerBtn.addEventListener('click',() => {
     burgerBtn.classList.toggle('is-open');
     headerContents.classList.toggle('is-open');
 });
+
+for(let i = 0; i < navLinks.length; i++) {
+    navLinks[i].addEventListener('click',() => {
+    burgerBtn.classList.remove('is-open');
+    headerContents.classList.remove('is-open');
+    });
+};
+
+headerContents.addEventListener('click',() => {
+    burgerBtn.classList.remove('is-open');
+    headerContents.classList.remove('is-open');
+});
+
+
 
 const questionTtls = document.querySelectorAll('.question-ttl');
 for(let i = 0; i<questionTtls.length; i++){
