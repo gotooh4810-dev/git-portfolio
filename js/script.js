20a7f5936586f24fe110c2d4470e5be5122b6e97
gotooh4810-dev <gotooh4810@gmail.com>
2026-03-30 20:52:41 +0900

    githubのソースコード貼り付け

--- js/script.js
+++ js/script.js
@@ -1,8 +1,15 @@
 const burgerBtn = document.querySelector('.burger-btn');
 const burgerArea = document.querySelector('.burger-area');
+const burgerLists = document.querySelectorAll('nav ul li');
 
 burgerBtn.addEventListener('click',() => {
     burgerBtn.classList.toggle('is-open');
     burgerArea.classList.toggle('is-open');
+});
 
+burgerLists.forEach((burgerList) => {
+    burgerList.addEventListener('click',() => {
+        burgerBtn.classList.remove('is-open');
+        burgerArea.classList.remove('is-open');
+    });
 });
